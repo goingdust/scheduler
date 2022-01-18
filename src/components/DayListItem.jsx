7c5fc0e4de04b4eeb991efb2ec27a8561b20ad13
goingdust <dustinluck@gmail.com>
2022-01-18 15:58:12 -0500

    Rename Application and Button to .jsx for react efficiency and wrote DayListItem component

---
+++ src/components/DayListItem.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function DayListItem(props) {
+  const { name, spots, setDay } = props;
+
+  return (
+    <li onClick={() => setDay('Tuesday')}>
+      <h2 className="text--regular">{name}</h2>
+      <h3 className="text--light">{spots} spots remaining</h3>
+    </li>
+  );
+}
