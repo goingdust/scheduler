b4c09d39790f7355cefbe2ccc2917654890f7747
goingdust <dustinluck@gmail.com>
2022-01-18 16:12:36 -0500

    Bring in styling for DayListItem and apply conditional classes for each component's state

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import classNames from "classnames";
+
+import 'components/DayListItem.scss';
 
 export default function DayListItem(props) {
-  const { name, spots, setDay } = props;
+  const { name, spots, setDay, selected } = props;
+
+  const dayClass = classNames('day-list__item', {'day-list__item--selected': selected}, {'day-list__item--full': !spots})
 
   return (
-    <li onClick={() => setDay('Tuesday')}>
+    <li className={dayClass} onClick={() => setDay('Tuesday')}>
       <h2 className="text--regular">{name}</h2>
       <h3 className="text--light">{spots} spots remaining</h3>
     </li>
