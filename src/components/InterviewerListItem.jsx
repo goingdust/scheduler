42db97c3dfed088a23118ed1f31a06906ebfd69b
goingdust <dustinluck@gmail.com>
2022-01-18 18:24:40 -0500

    Write InterviewerListItem component with props, its corresponding stories, and scss

---
+++ src/components/InterviewerListItem.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import classNames from "classnames";
+
+import 'components/InterviewerListItem.scss';
+
+export default function InterviewerListItem(props) {
+  const { id, name, avatar, setInterviewer, selected } = props;
+  
+  const interviewerClass = classNames(
+    'interviewers__item',
+    {'interviewers__item--selected': selected}
+  );
+
+  return (
+    <li onClick={() => setInterviewer(id)} className={interviewerClass}>
+      <img
+        className="interviewers__item-image"
+        src={avatar}
+        alt={name}
+      />
+      {selected && name}
+    </li>
+  );
+}
