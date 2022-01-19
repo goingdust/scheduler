e89797881b7feb8f04eadeb7a0ac82db57310d4a
goingdust <dustinluck@gmail.com>
2022-01-19 17:52:13 -0500

    Write story for Appointment Booked and Appointment Empty and import Header, Show, and Empty components into Appointment

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -186,4 +186,14 @@ storiesOf("Appointment", module)
       onSave={action("onSave")}
       onCancel={action("onCancel")}
     />
+  ))
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment
+        id={interviewer.id}
+        time="4pm"
+        interview={{student: "Lydia Miller-Jones", interviewer}}
+      />
+      <Appointment time="5pm" />
+    </Fragment>
   ))
