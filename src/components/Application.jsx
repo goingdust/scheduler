66bf025a738b2016a4d9ea329e3a58af4427b528
goingdust <dustinluck@gmail.com>
2022-01-20 18:43:25 -0500

    Write Promise.all in useEffect to do simultaneous get requests for days and appointments and then change the state with each of their data simultaneously

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -4,46 +4,8 @@ import axios from "axios";
 
 import DayList from "./DayList";
 import Appointment from "./Appointment";
+import getAppointmentsForDay from "helpers/selectors";
 import "components/Application.scss";
-
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-  }
-];
 
 export default function Application(props) {
   const [ state, setState ] = useState({
@@ -53,15 +15,23 @@ export default function Application(props) {
   });
 
   const setDay = day => setState({ ...state, day});
-  const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
-    axios
-      .get('/api/days')
-      .then((res) => setDays([...res.data]));
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+    ]).then(all => {
+      const days = all[0].data;
+      const appointments = all[1].data;
+      setState(prev => ({ ...prev, days, appointments }));
+    }).catch(err => {
+      console.log(err);
+    })
   }, []);
 
-  const parsedAppointments = appointments.map(appt => {
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
+  const parsedAppointments = dailyAppointments.map(appt => {
     return <Appointment key={appt.id} {...appt} />
   });
 
