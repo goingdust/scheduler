89ef08d61530fb4c0fe5ef7f3c20472681667f98
goingdust <dustinluck@gmail.com>
2022-01-19 20:43:22 -0500

    Bring Appointment component into Application and fake appointments data to see all appointments render on the page

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,7 +1,47 @@
 import React, { useState } from "react";
 
 import DayList from "./DayList";
+import Appointment from "./Appointment";
 import "components/Application.scss";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
 
 export default function Application(props) {
   const [ day, setDay ] = useState('');
@@ -23,6 +63,10 @@ export default function Application(props) {
       spots: 0,
     },
   ];
+
+  const parsedAppointments = appointments.map(appt => {
+    return <Appointment key={appt.id} {...appt} />
+  })
 
   return (
     <main className="layout">
@@ -47,7 +91,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {parsedAppointments}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
