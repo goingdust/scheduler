2c483162e4653ae452bf08ff14fb4685c3558239
goingdust <dustinluck@gmail.com>
2022-01-21 19:07:27 -0500

    Write useVisualMode hook and make it pass its corresponding tests

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+const useVisualMode = (initialMode) => {
+  const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode]);
+
+  const transition = (newMode, replace = false) => {
+    setHistory(prev => {
+      if (!replace) {
+        prev.push(newMode);
+      }
+      setMode(newMode);
+      return prev;
+    })
+  };
+
+  const back = () => {
+    setHistory(prev => {
+      if (prev.length >= 2) {
+        prev.pop();
+        setMode(prev[prev.length - 1]);
+      }
+      return prev;
+    });
+  };
+
+  return {
+    mode,
+    transition,
+    back
+  };
+};
+
+export default useVisualMode;
