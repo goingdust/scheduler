06ac17bc8fd6809bfd6d00a271ce3d949811c05f
goingdust <dustinluck@gmail.com>
2022-01-24 13:33:57 -0500

    Bring in Confirm component to show when the user clicks the delete button so that the true delete only happens when they confirm it/they can go back with cancel

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -5,16 +5,19 @@ import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
 import Status from "./Status";
+import Confirm from "./Confirm";
 import useVisualMode from "hooks/useVisualMode";
 import "./styles.scss";
 
 export default function Appointment(props) {
-  const { id, time, interview, interviewers, bookInterview, cancelInterview } = props;
+  const { id, time, interview, interviewers, bookInterview, cancelInterview } =
+    props;
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
   const SAVING = "SAVING";
   const DELETING = "DELETING";
+  const CONFIRM = "CONFIRM";
   const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
 
   const save = (name, interviewer) => {
@@ -24,18 +27,18 @@ export default function Appointment(props) {
     };
     transition(SAVING);
     bookInterview(id, interview).then(() => transition(SHOW));
-  }
+  };
 
   const deleteAppt = () => {
     transition(DELETING);
     cancelInterview(id).then(() => transition(EMPTY));
-  }
+  };
 
   return (
     <article className="appointment">
       <Header time={time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-      {mode === SHOW && <Show {...interview} onDelete={deleteAppt} />}
+      {mode === SHOW && <Show {...interview} onDelete={() => transition(CONFIRM)} />}
       {mode === CREATE && (
         <Form
           interviewers={interviewers}
@@ -46,6 +49,12 @@ export default function Appointment(props) {
       )}
       {mode === SAVING && <Status message="Saving" />}
       {mode === DELETING && <Status message="Deleting" />}
+      {mode === CONFIRM && (
+        <Confirm message="Are you sure you would like to delete?"
+          onConfirm={deleteAppt}
+          onCancel={back}
+        />
+      )}
     </article>
   );
 }
