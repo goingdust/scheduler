55917604a7f3b8fcafa04f723dc50d30a78f59a0
goingdust <dustinluck@gmail.com>
2022-01-25 16:11:06 -0500

    Write a test template for Appointment component and write 4 jest tests for the Form component

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -16,22 +16,23 @@ export default function Form(props) {
   const cancel = () => {
     reset();
     onCancel();
-  }
+  };
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
             value={student}
-            onChange={(event) => setStudent(event.target.value)}
+            onChange={event => setStudent(event.target.value)}
+            data-testid="student-name-input"
           />
         </form>
-        <InterviewerList 
+        <InterviewerList
           interviewers={interviewers}
           value={interviewer}
           onChange={setInterviewer}
@@ -39,8 +40,12 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={() => onSave(student, interviewer)}>Save</Button>
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={() => onSave(student, interviewer)}>
+            Save
+          </Button>
         </section>
       </section>
     </main>
